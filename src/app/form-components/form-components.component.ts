c14f0b038c7b5418a71ae0f7e47675c529b7f160
Prakruti <prakruti9902@gmail.com>
2024-08-06 21:21:54 +0530

    Initial commit

---
+++ src/app/form-components/form-components.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-components',
+  templateUrl: './form-components.component.html',
+  styleUrls: ['./form-components.component.css']
+})
+export class FormComponentsComponent {
+
+  
+
+  // submit(login){
+  //   console.log("Form Submitted",login)
+
+    submit(){
+      console.log("Form Submitted")
+
+  }
+}
+
